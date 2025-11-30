eb03c6537823f6d895ad9c21e8dfb36547306399
Rúben Silver <drogadinhosilver@gmail.com>
2025-11-30 11:07:17 +0100

    Create connect.js

---
+++ connect.js
@@ -0,0 +1,17 @@
+// Substituído com informações de Rúben Silver, proprietário do Uchiha-Bot 👾
+(function(_0x3e2e5c,_0x36de33){ 
+    // Dados do dono e bot atualizados
+    const ownerInformation = {
+        ownerName: "𝐑ú𝐛𝐞𝐧 𝐒𝐢𝐥𝐯𝐞𝐫 👨🏽‍💻",
+        botName: "𝐔𝐜𝐡𝐢𝐡𝐚-𝐁𝐨𝐭 👾",
+        botNumber: "+244955873027whatsApp.net",
+        ownerContact: "+244948963368whatsApp.net",
+        telegramLink: "t.me/uchiha_all",
+        instagramLink: "https://www.instagram.com/ruben_siilver?igsh=MWRpZjVwaXJwZ21kNQ==",
+        facebookLink: "https://www.facebook.com/profile.php?id=61551471087846",
+        apiKey: "EAAQuJmPAThYBQJVGWNL3cqm7CkotlWs..."
+    };
+
+    console.log(`Bot configurado e executado por: ${ownerInformation.ownerName}`);
+    console.log(`WhatsApp do Bot: ${ownerInformation.botNumber}`);
+});
